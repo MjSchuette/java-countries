b48b13bdae7af81ea1913b40e8d50371870ff622
MatthewSchuette <mjschuette4@gmail.com>
2020-07-10 19:13:56 -0500

    Finsihed names/start/u endpoint and population/total endpoint

--- src/main/java/com/lambdaschool/javacountries/controllers/CountryController.java
+++ src/main/java/com/lambdaschool/javacountries/controllers/CountryController.java
@@ -2,10 +2,12 @@ package com.lambdaschool.javacountries.controllers;
 
 import com.lambdaschool.javacountries.models.Country;
 import com.lambdaschool.javacountries.repositories.CountryRepository;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -16,6 +18,17 @@ public class CountryController {
     @Autowired
     CountryRepository countryrepos;
 
+    private List<Country> findCountries (List<Country> myList, CheckCountry tester){
+        List<Country> tempList = new ArrayList<>();
+        for (Country c : myList){
+            if (tester.test(c)){
+                tempList.add(c);
+            }
+        }
+        return tempList;
+    }
+
+    //    http://localhost:2019/names/all
     @GetMapping(value= "/names/all", produces = {"application/json"})
     public ResponseEntity<?> listAllCountries(){
         List<Country> myList = new ArrayList<>();
@@ -23,5 +36,34 @@ public class CountryController {
 
         myList.sort((e1, e2) -> e1.getName().compareToIgnoreCase(e2.getName()));
         return new ResponseEntity<>(myList, HttpStatus.OK);
+    }
+
+    //    http://localhost:2019/names/start/u
+    @GetMapping(value = "/names/start/{letter}", produces = "application/json")
+    public ResponseEntity<?> listALlByName(@PathVariable char letter){
+        List<Country> myList = new ArrayList<>();
+        countryrepos.findAll().iterator().forEachRemaining(myList::add);
+        List<Country> rtnList = findCountries(myList, e -> e.getName().charAt(0) == letter);
+
+        for ( Country c : rtnList){
+            System.out.println(c);
+        }
+        return new ResponseEntity<>(rtnList, HttpStatus.OK);
+    }
+
+    //    http://localhost:2019/population/total
+    @GetMapping(value = "/population/total", produces = "application/json")
+    public ResponseEntity<?> totalPopulation() {
+        List<Country> myList = new ArrayList<>();
+        countryrepos.findAll().iterator().forEachRemaining((myList::add));
+
+        long total = 0;
+        for (Country c : myList) {
+            total = total + c.getPopulation();
+        }
+
+        System.out.println("The total population is " + total);
+
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
